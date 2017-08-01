4f453ce2bd75563eac4bdf46f267b83b149886af
joel-g <joelaguerra1@gmail.com>
2017-07-31 23:07:24 -0700

    writes data to mysql server

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, json, request
 from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+data = cursor.fetchall()
+
+if len(data) is 0:
+    conn.commit()
+    return json.dumps({'message':'User created successfully !'})
+else:
+    return json.dumps({'error':str(data[0])})
+
 
 @app.route("/")
 def main():
@@ -17,6 +36,7 @@ def signUp():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
+    _hashed_password = generate_password_hash(_password)
 
     if _name and _email and _password:
         return json.dumps({'html':'<span>All fields good !!</span>'})
