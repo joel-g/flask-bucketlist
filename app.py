5cd9a52a33cbfee7fb234e10b02294684e7f08ed
joel-g <joelaguerra1@gmail.com>
2017-07-31 23:16:52 -0700

    refactor

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, json, request
 from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
+mysql = MySQL()
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -10,16 +11,6 @@ app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-conn = mysql.connect()
-cursor = conn.cursor()
-cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-data = cursor.fetchall()
-
-if len(data) is 0:
-    conn.commit()
-    return json.dumps({'message':'User created successfully !'})
-else:
-    return json.dumps({'error':str(data[0])})
 
 
 @app.route("/")
@@ -32,17 +23,32 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-    _hashed_password = generate_password_hash(_password)
+        if _name and _email and _password:
 
-    if _name and _email and _password:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
         return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-
+    finally:
+        curose.close()
+        conn.close()
 
 if __name__ == "__main__":
     app.run()
